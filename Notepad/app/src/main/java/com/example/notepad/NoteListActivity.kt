5172d5796e0a3f787ba27f8859f4b5c58312e6aa
Niraj Tiwari <tniraj7@gmail.com>
2020-02-20 20:52:25 +0900

    add new activity

---
+++ Notepad/app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -0,0 +1,21 @@
+package com.example.notepad
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_note_list.*
+
+class NoteListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+
+        }
+    }
+
+}
