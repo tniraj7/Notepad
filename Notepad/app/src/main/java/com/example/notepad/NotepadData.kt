b17a2cbe7a52d2fc9ebbbc87ff854514538674db
Niraj Tiwari <tniraj7@gmail.com>
2020-02-21 21:16:17 +0900

    transform data model classes into data  class

--- Notepad/app/src/main/java/com/example/notepad/NotepadData.kt
+++ Notepad/app/src/main/java/com/example/notepad/NotepadData.kt
@@ -1,10 +1,14 @@
 package com.example.notepad
 
-class CourseInfo(val courseId: String, val title: String) {
+data class CourseInfo(val courseId: String, val title: String) {
     override fun toString(): String {
         return title
     }
 }
 
-class NoteInfo(var course: CourseInfo, var title: String, var text: String)
+data class NoteInfo(var course: CourseInfo, var title: String, var text: String) {
+    override fun toString(): String {
+        return course.title
+    }
+}
 
