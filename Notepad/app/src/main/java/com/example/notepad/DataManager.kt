180cc51284cc699494d02d48105309dc587667a2
Niraj Tiwari <tniraj7@gmail.com>
2020-02-21 21:14:10 +0900

    Make DataManager as Singleton and initialize notes ArrayList

--- Notepad/app/src/main/java/com/example/notepad/DataManager.kt
+++ Notepad/app/src/main/java/com/example/notepad/DataManager.kt
@@ -1,12 +1,13 @@
 package com.example.notepad
 
-class DataManager {
+object DataManager {
     val courses = HashMap<String, CourseInfo>()
 
     val notes = ArrayList<NoteInfo>()
 
     init {
         initializeCourses()
+        initializeNotes()
     }
 
     private fun initializeCourses() {
@@ -19,7 +20,34 @@ class DataManager {
         course = CourseInfo(courseId = "Java_Lang", title = "Java Fundamentals: The Java Language")
         courses.set(course.courseId, course)
 
-        course = CourseInfo(title = "Java Fundamentals: The Core Platform", courseId = "Java_Core")
+        course = CourseInfo(title = "Java Core: The Core Platform", courseId = "Java_Core")
         courses.set(course.courseId, course)
+    }
+
+    private fun initializeNotes() {
+
+        var course = courses["Android_Intents"]!!
+        var note = NoteInfo(course, "Dynamic intent resolution",
+            "Wow, intents allow components to be resolved at runtime")
+        notes.add(note)
+
+
+        course = courses["Android_Async"]!!
+        note = NoteInfo(course, "Service default threads",
+            "Did you know that by default an Android Service will tie up the UI thread?")
+        notes.add(note)
+
+
+        course = courses["Java_Lang"]!!
+        note = NoteInfo(course, "Parameters",
+            "Leverage variable-length parameter lists")
+        notes.add(note)
+
+
+        course = courses["Java_Core"]!!
+        note = NoteInfo(course, "Compiler options",
+            "The -jar option isn't compatible with with the -cp option")
+        notes.add(note)
+
     }
 }
