da11225ee7862a1546ed0e25a3e408a8c5f1d5d1
Niraj Tiwari <tniraj7@gmail.com>
2020-02-23 18:03:24 +0900

    save the data into Bundle inside onSaveInstanceState and load the same data from bundle while creation of activity

--- Notepad/app/src/main/java/com/example/notepad/NoteActivity.kt
+++ Notepad/app/src/main/java/com/example/notepad/NoteActivity.kt
@@ -1,12 +1,10 @@
 package com.example.notepad
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
-import android.widget.Spinner
 
 import kotlinx.android.synthetic.main.activity_note.*
 import kotlinx.android.synthetic.main.content_note.*
@@ -33,7 +31,7 @@ class NoteActivity : AppCompatActivity() {
         // Apply the adapter to the spinner
         spinnerCourses.adapter = adapterCourses
 
-        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        notePosition = savedInstanceState?.getInt(NOTE_POSITION, POSITION_NOT_SET) ?: intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET)
 
         if (notePosition != POSITION_NOT_SET) {
             displayNote()
@@ -42,6 +40,12 @@ class NoteActivity : AppCompatActivity() {
             notePosition = DataManager.notes.lastIndex
         }
 
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(NOTE_POSITION, notePosition)
     }
 
     private fun displayNote() {
