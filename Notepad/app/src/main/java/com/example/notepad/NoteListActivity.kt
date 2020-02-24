2da5325f980406545a19a6f240a6ca8afb12c8b9
Niraj Tiwari <tniraj7@gmail.com>
2020-02-24 20:55:06 +0900

    remove listView from content_note_list.xml layout and add RecylcerView to the same

--- Notepad/app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ Notepad/app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -20,19 +20,15 @@ class NoteListActivity : AppCompatActivity() {
             startActivity(activityIntent)
         }
 
-        listNotes.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataManager.notes)
 
-        listNotes.setOnItemClickListener { parent, view, position, id ->
-            val activityIntent = Intent(this, NoteActivity::class.java)
-            activityIntent.putExtra(NOTE_POSITION, position)
-            startActivity(activityIntent)
-        }
+
+
     }
 
     override fun onResume() {
         super.onResume()
 
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
+       
     }
 
 }
