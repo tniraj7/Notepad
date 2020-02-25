01aa7e59932c3d10cb27260efbb025001145f297
Niraj Tiwari <tniraj7@gmail.com>
2020-02-25 20:19:08 +0900

    create custom Adapter class for Recycler View

---
+++ Notepad/app/src/main/java/com/example/notepad/NoteRecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.notepad
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRecyclerAdapter(private val context: Context):
+    RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>() {
+
+    private val layoutInflater = LayoutInflater.from(context)
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.item_note_list, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
